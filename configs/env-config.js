a177db252e826bae04a34c1730c2016516dd9231
HSU JUI SHENG <zed2786328@gmail.com>
2024-03-21 11:53:30 +0800

    optimize env config

--- configs/env-config.js
+++ configs/env-config.js
@@ -1,9 +1,9 @@
-import * as envConfigDev from "./env-config-dev.js";
-import * as envConfigProd from "./env-config-prod.js";
+import * as envConfigMock from "./env-config-mock.js";
+import * as envConfigReal from "./env-config-real.js";
 
 const envConfig =
-  process.env.NODE_ENV === "production" ? envConfigProd : envConfigDev;
+  process.env.NODE_ENV === "real" ? envConfigReal : envConfigMock;
 
 const { API_KEY, SECRET_KEY, REST_BASEURL, LINE_NOTIFY_TOKEN } = envConfig;
 
-export { API_KEY, LINE_NOTIFY_TOKEN, REST_BASEURL, SECRET_KEY };
+export { API_KEY, SECRET_KEY, REST_BASEURL, LINE_NOTIFY_TOKEN };
