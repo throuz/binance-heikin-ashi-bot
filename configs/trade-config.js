8405998b377998eedf71fc5665558641355c516f
Throuz <zed2786328@gmail.com>
2024-03-22 17:13:46 +0000

    change leverage

--- configs/trade-config.js
+++ configs/trade-config.js
@@ -1,6 +1,6 @@
 export const SYMBOL = "BTCUSDT";
 export const QUOTE_ASSET = "USDT";
-export const LEVERAGE = 10;
+export const LEVERAGE = 13;
 export const ORDER_AMOUNT_PERCENT = 100;
 export const LONG_TERM_KLINE_INTERVAL = "1d";
 export const KLINE_INTERVAL = "1h";
