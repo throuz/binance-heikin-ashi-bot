c5623930e5b3ddba2aa76c065329c7cfd3506303
Throuz <zed2786328@gmail.com>
2024-04-11 17:04:56 +0000

    change config

--- configs/trade-config.js
+++ configs/trade-config.js
@@ -4,5 +4,5 @@ export const LEVERAGE = 13;
 export const ORDER_AMOUNT_PERCENT = 100;
 export const LONG_TERM_KLINE_INTERVAL = "1d";
 export const KLINE_INTERVAL = "1h";
-export const AVERAGE_VOLUME_PERIOD = 25;
-export const AVERAGE_VOLUME_THRESHOLD_FACTOR = 0.25;
+export const AVERAGE_VOLUME_PERIOD = 29;
+export const AVERAGE_VOLUME_THRESHOLD_FACTOR = 0;
