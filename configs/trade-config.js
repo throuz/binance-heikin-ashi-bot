36ba7da93d60bca04887b79b3d072e0600adff2c
HSU JUI SHENG <zed2786328@gmail.com>
2024-07-06 11:34:57 +0800

    start st years ago

--- configs/trade-config.js
+++ configs/trade-config.js
@@ -13,6 +13,13 @@ export const AVG_VOL_PERIOD_SETTING = { min: 1, max: 50, step: 1 };
 export const OPEN_AVG_VOL_FACTOR_SETTING = { min: 0.5, max: 1, step: 0.05 };
 export const CLOSE_AVG_VOL_FACTOR_SETTING = { min: 1, max: 3, step: 0.05 };
 export const LEVERAGE_SETTING = { min: 1, max: 50, step: 1 };
-export const RANDOM_SAMPLE_NUMBER = 100000; // recommend 100000
-export const KLINE_START_TIME = null; // timestamp
-export const IS_KLINE_START_TIME_TO_NOW = false;
+export const RANDOM_SAMPLE_NUMBER = 1000; // recommend 100000
+export const KLINE_START_TIME = getTimestampYearsAgo(4); // timestamp
+export const IS_KLINE_START_TIME_TO_NOW = true;
+
+function getTimestampYearsAgo(years) {
+  const currentDate = new Date();
+  const targetYear = currentDate.getFullYear() - years;
+  currentDate.setFullYear(targetYear);
+  return currentDate.getTime();
+}
